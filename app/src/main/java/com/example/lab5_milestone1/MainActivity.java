42c750ddc546f79d6238e184579df9c2864a880f
lukas <lwehrli@wisc.edu>
2021-10-18 11:20:40 -0500

    Adding intent with new activity

--- app/src/main/java/com/example/lab5_milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab5_milestone1/MainActivity.java
@@ -2,9 +2,26 @@ package com.example.lab5_milestone1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void userLogin(View view){
+        EditText usernameField = (EditText) findViewById(R.id.username);
+        String username = usernameField.getText().toString();
+        loginTheUser(username);
+    }
+
+    public void loginTheUser(String username){
+        Intent intent = new Intent(this, userloggedin.class);
+        intent.putExtra("username", username);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
