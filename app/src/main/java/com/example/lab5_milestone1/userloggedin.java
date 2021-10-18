2f2dee432f4998ba68fb39ffb2ebe8a519aa8a4b
lukas <lwehrli@wisc.edu>
2021-10-18 12:18:30 -0500

    Added menu

--- app/src/main/java/com/example/lab5_milestone1/userloggedin.java
+++ app/src/main/java/com/example/lab5_milestone1/userloggedin.java
@@ -2,13 +2,51 @@ package com.example.lab5_milestone1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class userloggedin extends AppCompatActivity {
+
+    TextView displayMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userloggedin);
+
+        displayMsg =(TextView)findViewById(R.id.displaymsg);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("username");
+        displayMsg.setText("Welcome " + str + "!");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                System.out.println("Logout");
+                return true;
+            case R.id.addnote:
+                // do something
+                System.out.println("Add note");
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
